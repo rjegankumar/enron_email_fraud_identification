078643003a926e1fa2fe55aa5bd13cc01bd0ca71
Jegan Kumar Ramanathan <jeganram@umich.edu>
2017-06-06 12:19:49 +0530

    feat: Create scatter plots of new features vs POI

--- feature_selection.py
+++ feature_selection.py
@@ -26,12 +26,30 @@ def labels_features(feature1, feature2):
     labels, features = targetFeatureSplit(data)
     return np.array(labels), np.array(features)
 
+# creating labels and features for the new features mentioned above
 labels1, features1 = labels_features('total_payments', 'total_stock_value')
 labels2, features2 = labels_features('shared_receipt_with_poi', 'to_messages')
 labels3, features3 = labels_features('from_this_person_to_poi', 'from_messages')
 labels4, features4 = labels_features('from_poi_to_this_person', 'to_messages')
 
+# creating new features
 new_features1 = features1[:,0] + features1[:,1]
 new_features2 = features2[:,0] / features2[:,1]
 new_features3 = features3[:,0] / features3[:,1]
 new_features4 = features4[:,0] / features4[:,1]
+
+# defining a function to create scatter plots
+def scatter_plot(labels, features, feature_name):
+    plt.scatter(labels, 
+                features, 
+                s = 50, c = "b", alpha = 0.5)
+    plt.xlabel('poi')
+    plt.ylabel(feature_name)
+    plt.xticks(np.arange(0,1.5,1))
+    plt.show()
+
+# plotting new features vs poi    
+scatter_plot(labels1, new_features1, "total payments and stock value")
+scatter_plot(labels2, new_features2, "shared poi receipt/ to messages")
+scatter_plot(labels3, new_features3, "to poi/ from messages")
+scatter_plot(labels4, new_features4, "from poi/ to messages")
