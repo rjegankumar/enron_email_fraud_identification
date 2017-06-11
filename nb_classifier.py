458200a759b5f24a7118cd1b80a30c1b534cc46b
Jegan Kumar Ramanathan <jeganram@umich.edu>
2017-06-11 18:51:36 +0530

    fix: Change nb classifier code to use gridsearchCV to select features

--- nb_classifier.py
+++ nb_classifier.py
@@ -3,6 +3,10 @@ import pickle
 from sklearn.naive_bayes import GaussianNB
 from sklearn.cross_validation import StratifiedShuffleSplit
 from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import SelectKBest
+from sklearn.grid_search import GridSearchCV
+from collections import Counter
 import numpy as np
 
 # loading the enron data dictionary
@@ -12,73 +16,59 @@ with open("final_project_dataset.pkl", "r") as data_file:
 # removing 'TOTAL' outlier
 del data_dict['TOTAL']
 
-# function to train and predict POIs based on the features provided as input
-def nbclassifier(feat_list):
-    # creating list of labels and list of numpy arrays containing the features
-    data = featureFormat(data_dict, feat_list, sort_keys = True)
-    labels, features = targetFeatureSplit(data)
-
-    # Fitting and testing Gaussian Naive Bayes Classifier
-    accuracy = []
-    precision = []
-    recall = []
-    f1 = []
-
-    sss = StratifiedShuffleSplit(labels, 1000, random_state = 1)
-
-    for train_indices, test_indices in sss:
-        features_train = [features[i] for i in train_indices]
-        features_test = [features[j] for j in test_indices]
-        labels_train = [labels[i] for i in train_indices]
-        labels_test = [labels[j] for j in test_indices]
-
-        clf = GaussianNB()
-        clf.fit(features_train, labels_train)
-        pred = clf.predict(features_test)
-
-        accuracy.append(clf.score(features_test, labels_test))
-        precision.append(precision_score(labels_test, pred))
-        recall.append(recall_score(labels_test, pred))
-        f1.append(f1_score(labels_test, pred))
-
-    print "\n\nNB Classifier Result with features - "
-    for f in feat_list[1:]:
-        print f
-    print "\naccuracy:", np.mean(accuracy)
-    print "precision:", np.mean(precision)
-    print "recall:", np.mean(recall)
-    print "f1 score:", np.mean(f1)
-
-# selecting only 2 features - total_stock_value and bonus for now
-# total_stock_value - data available for all POIs and second best feature
-# bonus - data available for 16 out of 18 POIs and third best feature
-nbclassifier(['poi',
-              'total_stock_value',
-              'bonus'])
-
-# changing features list to replace total_stock_value with
-# exercised_stock_option
-nbclassifier(['poi',
-              'exercised_stock_options',
-              'bonus'])
-
-# Adding saalary to the features list
-nbclassifier(['poi',
+# list containing all labels and features except email
+feat_list = ['poi',
+              'salary',
+              'to_messages',
+              'deferral_payments',
+              'total_payments',
               'exercised_stock_options',
               'bonus',
-              'salary'])
+              'restricted_stock',
+              'shared_receipt_with_poi',
+              'restricted_stock_deferred',
+              'total_stock_value',
+              'expenses',
+              'loan_advances',
+              'from_messages',
+              'other',
+              'from_this_person_to_poi',
+              'director_fees',
+              'deferred_income',
+              'long_term_incentive',
+              'from_poi_to_this_person']
 
-# Removing everything except exercised stock options
-nbclassifier(['poi',
-              'exercised_stock_options'])
-'''
-Provided the best results with the highest f1 score
-'''
+# Selecting the best features using GridSearchCV
+data = featureFormat(data_dict, feat_list, sort_keys = True)
+labels, features = targetFeatureSplit(data)
 
-# Replacing exercised stock options with bonus
-nbclassifier(['poi',
-              'bonus'])
+best_params = []
+best_score = []
+best_features = []
 
-# Replacing exercised stock options with bonus
-nbclassifier(['poi',
-              'salary'])
+sss = StratifiedShuffleSplit(labels, 1000, random_state = 1)
+
+for train_indices, test_indices in sss:
+    features_train = [features[i] for i in train_indices]
+    features_test = [features[j] for j in test_indices]
+    labels_train = [labels[i] for i in train_indices]
+    labels_test = [labels[j] for j in test_indices]
+
+    pipe = Pipeline([('KBest', SelectKBest()),
+                    ('clf', GaussianNB())])
+    K = [1,2,3,4,5,6]
+    param_grid = [{'KBest__k': K}]
+
+    gs = GridSearchCV(estimator=pipe, param_grid=param_grid, scoring='f1')
+    gs.fit(features_train, labels_train)
+
+    best_params.append(gs.best_params_['KBest__k'])
+    best_score.append(gs.best_score_)
+
+    kb = SelectKBest(k=gs.best_params_['KBest__k'])
+    kb.fit(features_train, labels_train)
+    best_features.append(list(kb.get_support(indices=True)+1))
+
+print "Best f1 score:", max(best_score)
+print "No. of features used for the best f1 score:", best_params[best_score.index(max(best_score))]
+print "Names of features used:\n", [feat_list[i] for i in best_features[best_score.index(max(best_score))]]
