de72a38fd6fb0c34f25838c03a2405435d6a6e73
Jegan Kumar Ramanathan <jeganram@umich.edu>
2017-06-11 19:40:49 +0530

    fix: Include code that outputs final nb classifier metrics

--- nb_classifier.py
+++ nb_classifier.py
@@ -2,11 +2,10 @@ from feature_format import featureFormat, targetFeatureSplit
 import pickle
 from sklearn.naive_bayes import GaussianNB
 from sklearn.cross_validation import StratifiedShuffleSplit
-from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.pipeline import Pipeline
 from sklearn.feature_selection import SelectKBest
 from sklearn.grid_search import GridSearchCV
-from collections import Counter
 import numpy as np
 
 # loading the enron data dictionary
@@ -46,7 +45,7 @@ best_params = []
 best_score = []
 best_features = []
 
-sss = StratifiedShuffleSplit(labels, 1000, random_state = 1)
+sss = StratifiedShuffleSplit(labels, 1000, random_state = 42)
 
 for train_indices, test_indices in sss:
     features_train = [features[i] for i in train_indices]
@@ -72,3 +71,37 @@ for train_indices, test_indices in sss:
 print "Best f1 score:", max(best_score)
 print "No. of features used for the best f1 score:", best_params[best_score.index(max(best_score))]
 print "Names of features used:\n", [feat_list[i] for i in best_features[best_score.index(max(best_score))]]
+
+final_feat_list = ['poi', 'exercised_stock_options', 'bonus', 'total_stock_value']
+
+# Computing evaluation metrics using the selected features
+final_data = featureFormat(data_dict, final_feat_list, sort_keys = True)
+final_labels, final_features = targetFeatureSplit(final_data)
+
+final_sss = StratifiedShuffleSplit(final_labels, 1000, random_state = 42)
+
+accuracy = []
+precision = []
+recall = []
+f1 = []
+
+for train_indices, test_indices in final_sss:
+    features_train = [final_features[i] for i in train_indices]
+    features_test = [final_features[j] for j in test_indices]
+    labels_train = [final_labels[i] for i in train_indices]
+    labels_test = [final_labels[j] for j in test_indices]
+
+    clf = GaussianNB()
+    clf.fit(features_train, labels_train)
+    pred = clf.predict(features_test)
+
+    accuracy.append(accuracy_score(labels_test, pred))
+    precision.append(precision_score(labels_test, pred))
+    recall.append(recall_score(labels_test, pred))
+    f1.append(f1_score(labels_test, pred))
+
+print "Evaluation results of GaussianNB using best features:"
+print "Mean Accuracy:", np.mean(accuracy)
+print "Mean Precision:", np.mean(precision)
+print "Mean Recall:", np.mean(recall)
+print "Mean f1 score:", np.mean(f1)
